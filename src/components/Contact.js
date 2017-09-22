dbfe7a36d3868decbc464ccb3b89f76bca0d3146
Joseph Gordy <jgordy2424@gmail.com>
2017-09-22 14:48:09 -0400

    about us page rendering correctly with content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
   render() {
     return(
-      <div className='Home'>
+      <div className='Contact'>
         <h1>Contact Page</h1>
+        <a href="#"><i className="fa fa-facebook-square" aria-hidden="true"></i></a>
+        <a href="#"><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
       </div>
     );
   }
