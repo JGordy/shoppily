12315ad7a032b2c9244f9665cd2adb6369b74a91
Joseph Gordy <jgordy2424@gmail.com>
2017-09-22 20:36:39 -0400

    changed active page styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,7 @@ export default class Contact extends Component {
             <a href="#"><i className="fa fa-facebook-square" aria-hidden="true"></i><span className="social_span">Facebook</span></a>
             <a href="#"><i className="fa fa-twitter-square" aria-hidden="true"></i><span className="social_span">Twitter</span></a>
             <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i><span className="social_span">Instagram</span></a>
+            <a href="#"><img src="https://www.google.com/search?q=instagram+logo&rlz=1C5CHFA_enUS742US743&source=lnms&tbm=isch&sa=X&ved=0ahUKEwilp5bIhbrWAhWIMyYKHWbqAnIQ_AUICigB&biw=1244&bih=629#imgrc=B1mxnjLnIfSk1M:" alt=""/></a>
           </div>
         </div>
         <div className="email_link">
