cab686e7b61bc88a2091bfa4efa2bd90b9b1d25c
Joseph Gordy <jgordy2424@gmail.com>
2017-09-25 08:50:33 -0400

    Added descriptions for all products in the store. Buttons created on the home page for all brands

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
@@ -6,13 +7,16 @@ export default class Home extends Component {
       <div className='Home'>
         <div>
           <img src={require("../images/home/Goog_family.png")} alt=""/>
+          <Link className="shop_google" to='/google'>Shop Google Products</Link>
         </div>
         <div>
           <img
           src={require('../images/home/MS_family.png')} alt=""/>
+          <Link className="shop_ms" to='/microsoft'>Shop Microsoft Products</Link>
         </div>
         <div>
           <img src={require('../images/home/Apple_family.png')} alt=""/>
+          <Link className="shop_apple" to='/apple'>Shop Apple Products</Link>
         </div>
       </div>
     );
--- src/data/data.js
+++ src/data/data.js
@@ -3,16 +3,19 @@ let products = {
     {
       name: "iPhone X",
       price: "$999.99",
+      specs: "It’s all screen, the first OLED screen Designed for iPhone X, Face Id: A revolution in recognition",
       pic_url: "https://cdn.macrumors.com/article-new/2017/09/iphonexcolors-800x573.jpg"
     },
     {
       name: "MacBook Pro w/Touch bar",
       price: "$1799.99",
+      specs: "Up to 10 hours of battery life, Touch Bar and Touch Id, Speedy SSD storage, Snap to everything",
       pic_url: "https://www.apple.com/v/macbook-pro/n/images/overview/og.png?201709081552"
     },
     {
       name: "Apple dongle collection",
       price: "$199.99",
+      specs: "For the Apple die hards: A full set of dongles needed for your new iPhone, iPad, or Macbook",
       pic_url: "https://brunocapuano.files.wordpress.com/2016/10/cv3fffsvuaamax6.jpg"
     }
   ],
@@ -20,16 +23,19 @@ let products = {
     {
       name: "Surface Pro 4",
       price: "$899.99",
+      specs: "Laptop power and performance with 6th Gen Intel Core processor, 12.3-inch PixelSense Display touchscreen, Surface Pen included",
       pic_url: "https://hniesfp.imgix.net/8/images/detailed/49/computers_surface4_final_product_2_7129-zb.jpg?fit=fill&bg=0FFF&w=1500&h=1000&auto=format,compress"
     },
     {
-      name: "Surface Keyboard",
+      name: "Surface Pro Signature Typecover",
       price: "$129.99",
+      specs: "Constructed with Alcantara, a luxury fabric sourced from Italy, Surface Pro Signature Type Cover offers a perfect blend of sophisticated style, comfort, and technology.",
       pic_url: "https://c.s-microsoft.com/en-us/CMSImages/SurfacePro4_Innovation_10_FeaturePanel_V2.jpg?version=1f03abf6-8f3d-eac0-79c6-73c5d0046161"
     },
     {
       name: "Xbox One X",
       price: "$499.99",
+      specs: "Experience immersive true 4K gaming, Play on Xbox Live, the most advanced multiplayer network, Games run smoothly, look amazing, and load quickly on 1080p screens",
       pic_url: "https://cdn0.vox-cdn.com/uploads/chorus_asset/file/8667595/xboxonex.jpg"
     }
   ],
